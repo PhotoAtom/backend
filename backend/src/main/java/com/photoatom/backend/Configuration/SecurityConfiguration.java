1fc9d8b5f14dcc83340dd1f682498f8daf24168b
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2024-12-22 09:19:56 +0530

    Remove KC Certs References

--- backend/src/main/java/com/photoatom/backend/Configuration/SecurityConfiguration.java
+++ backend/src/main/java/com/photoatom/backend/Configuration/SecurityConfiguration.java
@@ -1,7 +1,5 @@
 package com.photoatom.backend.Configuration;
 
-import org.springframework.beans.factory.annotation.Value;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,27 +15,13 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
-import org.springframework.web.client.RestTemplate;
-
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
-
-import javax.net.ssl.KeyManagerFactory;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManagerFactory;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-
-import java.security.GeneralSecurityException;
-import java.security.KeyStore;
 
 import java.util.Collection;
 import java.util.List;
@@ -47,25 +31,6 @@ import java.util.List;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfiguration {
-
-  @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-  String issuerUri;
-
-  // Keycloak CA Certificate Path
-  @Value("${keycloak.caCertPath}")
-  private String caCertPath;
-
-  // Keycloak CA Certificate Password
-  @Value("${keycloak.caCertPassword}")
-  private String caCertPassword;
-
-  // Keycloak User Certificate Path
-  @Value("${keycloak.userCertPath}")
-  private String userCertPath;
-
-  // Keycloak User Certificate Password
-  @Value("${keycloak.userCertPassword}")
-  private String userCertPassword;
 
   interface AuthoritiesConverter extends Converter<Map<String, Object>, Collection<GrantedAuthority>> {
   }
@@ -114,29 +79,6 @@ public class SecurityConfiguration {
   }
 
   /**
-   * Override the JWT Decode process by supplying
-   * Keycloak Ingress Certificates to use against
-   * the Issuer URI.
-   *
-   * @return
-   * @throws IOException
-   * @throws GeneralSecurityException
-   */
-  @Bean
-  JwtDecoder jwtDecoder() throws IOException, GeneralSecurityException {
-
-    // Load keycloak SSL certificates
-    SSLContext sslSocket = createSslSocketFactory(caCertPath, caCertPassword, userCertPath, userCertPassword);
-
-    // For REST Operations, attach the SSL certificates to ensure SSL communication
-    RestTemplate restOperations = new RestTemplate(new CustomRequestFactory(sslSocket));
-
-    // Return the Decoder with Issuer URI and configured REST Operations
-    return NimbusJwtDecoder.withIssuerLocation(issuerUri)
-        .restOperations(restOperations).build();
-  }
-
-  /**
    * OAuth2 Resource Server Authentication Implementation
    *
    * @param http
@@ -170,39 +112,6 @@ public class SecurityConfiguration {
 
     // Return all authentication rules
     return http.build();
-  }
-
-  /**
-   * Generate SSL Context for the provided certificates path and passwords.
-   *
-   * @param caCertPath       CA Certificate Path
-   * @param caCertPassword   CA Certificate Password
-   * @param userCertPath     User Certificate Path
-   * @param userCertPassword User Certificate Password
-   * @return SSLContext
-   * @throws IOException
-   * @throws GeneralSecurityException
-   */
-  private SSLContext createSslSocketFactory(
-      String caCertPath, String caCertPassword, String userCertPath, String userCertPassword)
-      throws IOException, GeneralSecurityException {
-
-    KeyStore keyStore = KeyStore.getInstance("pkcs12");
-    keyStore.load(new FileInputStream(userCertPath), userCertPassword.toCharArray());
-
-    KeyStore trustStore = KeyStore.getInstance("jks");
-    trustStore.load(new FileInputStream(caCertPath), caCertPassword.toCharArray());
-
-    TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("X509");
-    trustManagerFactory.init(trustStore);
-
-    KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("PKIX");
-    keyManagerFactory.init(keyStore, userCertPassword.toCharArray());
-
-    SSLContext sslContext = SSLContext.getInstance("SSL");
-    sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
-
-    return sslContext;
   }
 
 }
