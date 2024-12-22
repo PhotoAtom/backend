f52beaf0329987e19bf44c7118e49098e32c754d
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2024-12-22 10:41:37 +0530

    Keycloak Certs No Longer Required (#21)
    * Keycloak Certs No Longer Required
    
    * POM Update
    
    * Remove KC Certs
    
    * Remove KC Certs References
    
    * Removing not required files
    
    * Dockerfile Update

--- backend/src/main/java/com/photoatom/backend/Configuration/ValkeyConfiguration.java
+++ backend/src/main/java/com/photoatom/backend/Configuration/ValkeyConfiguration.java
@@ -31,13 +31,10 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
 
-import lombok.extern.slf4j.Slf4j;
-
 /**
  * Configuration class for Valkey Connection using Jedis Library
  */
 @Configuration
-@Slf4j
 public class ValkeyConfiguration {
 
   // Valkey Host
@@ -79,8 +76,6 @@ public class ValkeyConfiguration {
   @Bean
   JedisConnectionFactory jedisConnectionFactory() throws GeneralSecurityException, IOException {
 
-    log.info("Setting up Valkey Configuration");
-
     // Inserting Valkey configuration details as Redis Configuration.
     RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
 
@@ -88,14 +83,13 @@ public class ValkeyConfiguration {
     redisStandaloneConfiguration.setPort(redisPort);
     redisStandaloneConfiguration.setPassword(RedisPassword.of(redisPassword));
 
-    log.info("Completed Valkey Configuration");
-
-    log.info("Setting up Jedis Configuration");
-
     // Generating SSL Factory for creating TLS connections with Valkey Database.
     // Generate Truststore and Keystore using:
-    // openssl pkcs12 -export -in certificates/cache/tls.crt -inkey certificates/cache/tls.key -out certificates/cache/valkeystore.p12 -name "redis"
-    // keytool -importcert -keystore certificates/cache/truststore.jks -storepass changeit -file certificates/cache/ca.crt
+    // openssl pkcs12 -export -in certificates/cache/tls.crt -inkey
+    // certificates/cache/tls.key -out certificates/cache/valkeystore.p12 -name
+    // "redis"
+    // keytool -importcert -keystore certificates/cache/truststore.jks -storepass
+    // changeit -file certificates/cache/ca.crt
     SSLSocketFactory sslFactory = createSslSocketFactory(caCertPath, caCertPassword, userCertPath, userCertPassword);
 
     // Specifying SSL Connection configuration
@@ -105,8 +99,6 @@ public class ValkeyConfiguration {
     // Forming connection factory using both Jedis and Redis configuration.
     JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(redisStandaloneConfiguration,
         jedisClientConfiguration);
-
-    log.info("Completed Jedis Configuration");
 
     return jedisConnectionFactory;
   }
@@ -121,12 +113,8 @@ public class ValkeyConfiguration {
   @Bean
   RedisTemplate<String, Object> redisTemplate() throws GeneralSecurityException, IOException {
 
-    log.info("Setting up Redis Template Configuration");
-
     RedisTemplate<String, Object> template = new RedisTemplate<>();
     template.setConnectionFactory(jedisConnectionFactory());
-
-    log.info("Completed Redis Template Configuration");
 
     return template;
   }
@@ -140,16 +128,8 @@ public class ValkeyConfiguration {
   @Bean
   public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
 
-    log.info("Validating Connection with Valkey Database");
-
+    connectionFactory.getConnection().echo("Connection ECHO".getBytes(StandardCharsets.UTF_8));
     // Validating connection with the Valkey database.
-    log.info("Connection ECHO: "
-        + new String(connectionFactory.getConnection().echo("Connection ECHO".getBytes(StandardCharsets.UTF_8)),
-            StandardCharsets.UTF_8));
-
-    log.info("Validated Connection with Valkey Database");
-
-    log.info("Setting up Cache Configuration");
 
     // Setting cache configuration such as cache name, TTL duration and
     // serialization.
@@ -157,8 +137,6 @@ public class ValkeyConfiguration {
         .prefixCacheNameWith(this.getClass().getPackageName() + ".").entryTtl(Duration.ofMinutes(15))
         .serializeValuesWith(SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
         .disableCachingNullValues();
-
-    log.info("Completed Cache Configuration");
 
     return RedisCacheManager.builder(connectionFactory).enableStatistics().cacheDefaults(cacheConfiguration).build();
 
